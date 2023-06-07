60a4067f76b55af27213f7a647c5c64e36fc8e31
Izzatilla <108586467+01Izzat@users.noreply.github.com>
2023-06-07 19:35:17 +0500

    login,register

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import { icon } from "../constans";
+import { Input } from "../ui";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  return (
+    <div className="text-center mt-5">
+      <main className="form-signin w-25 m-auto">
+        <form>
+          <img className="mb-2" src={icon} alt="" width="72" height="60" />
+          <h1 className="h3 mb-3 fw-normal">Please login</h1>
+
+          <Input label={"Email address"} state={email} setState={setEmail} />
+          <Input
+            label={"Password"}
+            type={"password"}
+            state={password}
+            setState={setPassword}
+          />
+
+          <button className="w-100 btn btn-lg btn-primary mt-2" type="submit">
+            Login
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default Login;
--- src/constans/index.js
+++ src/constans/index.js
@@ -1,2 +1,4 @@
 export const logo =
   "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Mercedes-Benz_Logo_2010.svg/2560px-Mercedes-Benz_Logo_2010.svg.png";
+export const icon =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Mercedes-Logo.svg/2048px-Mercedes-Logo.svg.png";
